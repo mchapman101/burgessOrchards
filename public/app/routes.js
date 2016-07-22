fa334b30dd87929525ca2c70c964ce441ae6a079
MarkChapman <mchapman101@yahoo.com>
2016-07-22 15:42:23 -0600

    fixed thumbnail alignment, added signup modal and form and other style fixes

--- public/app/routes.js
+++ public/app/routes.js
@@ -45,11 +45,6 @@ angular.module("burgessOrchards").config(function($stateProvider, $urlRouterProv
       templateUrl: 'public/app/components/recipies/recipies.html',
       controller: 'recipiesCtrl'
     })
-    .state('signup', {
-      url: '/signup',
-      templateUrl: 'public/app/components/signup/signupTmpl.html',
-      controller: 'signupCtrl'
-    });
 
 
   // ASSIGN OTHERWISE
