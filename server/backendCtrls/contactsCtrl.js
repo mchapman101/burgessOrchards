bb85dd38da9cb6216d10453d415cc4fdacff1475
preston <mechampreston@gmail.com>
2017-08-17 20:24:16 -0600

    Fixed update email button

--- server/backendCtrls/contactsCtrl.js
+++ server/backendCtrls/contactsCtrl.js
@@ -26,8 +26,8 @@ module.exports = {
     update: function(req, res) {
         db.contacts.update({
             id: req.params.id,
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
+            first_name: req.body.firstname,
+            last_name: req.body.lastname,
             email: req.body.email
         }, function(err, result) {
             if (err) {
